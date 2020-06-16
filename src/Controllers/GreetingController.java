2d7d01afe4e09a4b02486f887443572b13d6eac2
longsugar88 <mrlongdung@gmail.com>
2020-06-16 21:40:58 +0700

    up

---
+++ src/Controllers/GreetingController.java
@@ -0,0 +1,15 @@
+package Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping ("/greating")
+    public String greeting(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
---
+++ web/WEB-INF/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mr Sugar
+  Date: 6/16/2020
+  Time: 3:57 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello ${name}</h1>
+</body>
+</html>
